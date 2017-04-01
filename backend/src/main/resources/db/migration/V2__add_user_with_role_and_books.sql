84408537d16fc1382737d2342023a1ca639b9b91
Mateusz <mateusz.sioja@gmail.com>
2017-04-01 23:53:51 +0200

    fix

--- backend/src/main/resources/db/migration/V2__add_user_with_role_and_books.sql
+++ backend/src/main/resources/db/migration/V2__add_user_with_role_and_books.sql
@@ -1,13 +1,13 @@
 INSERT INTO person VALUES
-  (1, 'Mateusz', 'Sioja', 'admin', '$2a$10$0kzdNMnHDYU1GQ3geJEGSec4hvrrVdkgogiJOsJwICJQqJSswTkK6' );
+  (1001, 'Mateusz', 'Sioja', 'admin', '$2a$10$0kzdNMnHDYU1GQ3geJEGSec4hvrrVdkgogiJOsJwICJQqJSswTkK6' );
 
 INSERT INTO role VALUES
   (1, 'ROLE_ADMIN'),
   (2, 'ROLE_USER');
 
 INSERT INTO book VALUES
-  (1, '2154886', 'J.K. Rowling', 'Harry Potter', 'Fantasy', 1),
-  (2, '2124886', 'Coben Harlan', 'Hole', 'Horror', 1);
+  (1001, '2154886', 'J.K. Rowling', 'Harry Potter', 'Fantasy', 1001),
+  (1002, '2124886', 'Coben Harlan', 'Hole', 'Horror', 1001);
 
 INSERT INTO user_role VALUES
-  (1, 1);
+  (1001, 1);
